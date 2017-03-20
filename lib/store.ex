ab79930fe6bfee0bb0f6698f803b691d5b7cbd3d
charlesoft <charlees1509@hotmail.com>
2017-03-19 22:29:33 -0300

    Store applications created with basics functionalities.

---
+++ lib/store.ex
@@ -0,0 +1,37 @@
+defmodule Store do
+
+  @doc """
+  gets rows, parses their prices and
+  passes an argument to sum private function
+  """
+  def sum_prices(rows) do
+    Enum.map(rows, &parse_price(&1))
+    |> sum
+  end
+
+  def find_eletronic([%{"name" => name, "category" => "Eletronic"} | _]) do
+    name
+  end
+
+  def find_eletronic([_| tail]) do
+    find_eletronic(tail)
+  end
+
+  def find_eletronic([]) do
+    raise "No product found"
+  end
+
+
+  defp parse_price(%{ "price" => price }) do
+    price
+    |> String.to_float
+    |> abs
+  end
+
+  @doc """
+  gets rows and sum all the prices
+  """
+  defp sum(rows) do
+    Enum.reduce(rows, fn(x,acc) -> x + acc end)
+  end
+end
---
+++ test/store_test.exs
@@ -0,0 +1,23 @@
+defmodule StoreTest do
+  use ExUnit.Case
+  doctest Store
+
+  setup do
+    IO.puts "List created!"
+
+    [list: [
+             %{"name" => "Programing Elixir", "price" => "10.00", "category" => "Book"},
+             %{"name" => "Ruby on Rails", "price" => "59.90", "category" => "Book"},
+             %{"name" => "Kindle", "price" => "90.0", "category" => "Eletronic"}
+           ]]
+  end
+
+
+  test "sum product prices", context do
+    assert Store.sum_prices(context[:list]) == 159.90
+  end
+
+  test "return eletronic product", context do
+    assert Store.find_eletronics(context[:list]) == "Kindle"
+  end
+end
